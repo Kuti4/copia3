556951e4b036d26a0df7a645bbb5270f760936df
ytopec <ilja.sem4enko@yandex.ru>
2022-11-22 17:56:14 +0500

    примерно регистрация

---
+++ reglog/forms.py
@@ -0,0 +1,35 @@
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+def email_validate(value):
+      if User.objects.filter(email=value).exists():
+        raise forms.ValidationError(
+            'Такая почта уже зарегистрирована!',
+            params={'value': value},
+        )  
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}), validators=[email_validate])
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Еще раз', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    
+
+    class Meta:
+      model = User
+      fields = ('username', 'email', 'password1', 'password2')
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField()
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
--- tuning/urls.py
+++ tuning/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from reglog import views as reglogin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', reglogin.register, name='register'),
+    path('login/', reglogin.login, name='login'),
 ]
