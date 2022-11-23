911bc1e6c9d7780e4644b828ab36468fe9fecdb6
ytopec <ilja.sem4enko@yandex.ru>
2022-11-23 15:43:53 +0500

    не робит extends

--- tuning/settings.py
+++ tuning/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'tuning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'comments/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
